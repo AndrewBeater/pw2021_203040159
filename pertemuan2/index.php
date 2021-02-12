a9bd67e921f725b64e5867ce81bd5515295cb27f
AndrewBeater <mandrewrrizanta@gmail.com>
2021-02-12 14:25:20 +0700

    menyelesaikan video pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,67 @@
+<?php
+/*
+Muhammad Andrew Rahdi Rizanta
+203040159
+https://github.com/AndrewBeater/pw2021_203040159
+Pertemuan 2 - 12 Februari 2021
+Mempelajari mengenai sintaks Dasar PHP
+*/
+?>
+
+<?php
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
+// Standar Output
+// echo, print
+// print_r
+// var_dump
+
+// Penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// variable dan Tipe Data
+// variabel
+// tidak boleh diawali dengan angka. tapi boleh mengandung angka
+// $nama = "Muhammad Andrew";
+// echo "Halo, nama saya $nama";
+
+// operator
+// aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// penggabung string / cancatenation / concat
+// .
+// $nama_depan = "Muhammad";
+// $nama_belakang = "Andrew";
+// echo $nama_depan . " " . $nama_belakang;
+
+// assignment
+// =, +=, -=, *=, /=, %=, .=
+// $x = 1;
+// $x -= 5;
+// echo $x;
+// $nama = "Muhammad";
+// $nama .= " ";
+// $nama .= "Andrew";
+// echo $nama;
+
+// perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump(1 == "1");
+
+// identitas
+// ===, !==
+// var_dump(1 === "1");
+
+// logika
+// &&, ||, !
+// $x = 30;
+// var_dump($x < 20 || $x % 2 == 0);
+
+
+?>
