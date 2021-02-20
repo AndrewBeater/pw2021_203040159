957ce1733faa779d2650ed7fd3f6cb7fdb40ace9
AndrewBeater <mandrewrrizanta@gmail.com>
2021-02-20 11:54:15 +0700

    menyelesaikan video pertemuan 3

---
+++ pertemuan3/latihan2.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Muhammad Andrew Rahdi Rizanta
+203040159
+https://github.com/AndrewBeater/pw2021_203040159
+Pertemuan 3 - 20 Februari 2021
+Mempelajari mengenai struktur kendali php
+*/
+?>
+
+<?php
+// Pengkondisian / percabangan
+// if else
+// if else if else
+// ternary
+// switch
+
+$x = 20;
+if($x < 20){
+    echo "benar";
+} else if($x == 20){
+    echo "bingo!";
+} else {
+    echo "salah";
+}
+
+
+
+
+
+
+
+
+
+
+?>
