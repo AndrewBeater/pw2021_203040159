8fa776952b0eadab496e24b517295beaf4e83643
AndrewBeater <mandrewrrizanta@gmail.com>
2021-02-26 22:15:26 +0700

    menyelesaikan video pertemuan 4

---
+++ pertemuan4/date.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Muhammad Andrew Rahdi Rizanta
+203040159
+https://github.com/AndrewBeater/pw2021_203040159
+Pertemuan 4 - 26 Februari 2021
+Mempelajari mengenai function dalam php
+*/
+?>
+
+<?php
+// Date
+// Untuk menampilkan tanggal dengan format tertentu
+// echo date("l, d-M-Y");
+
+// Time
+// UNIX Timestamp / EPOCH time
+// detik yang sudah berlalu sejak 1 Januari 1970
+// echo time();
+// echo date("l", time()-60*60*24*100);
+
+// mktime
+// membuat sendiri detik
+// mktime(0,0,0,0,0,0)
+// jam, menit, detik, bulan, tanggal, tahun)
+// echo date("l", mktime(0,0,0,9,23,2001));
+
+// strtotime
+// echo date("l", strtotime("23 sep 2001"));
+
+
+
+
+
+
+?>
