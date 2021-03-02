2644b5704b48653f3c07468322176c743cbbcead
AndrewBeater <mandrewrrizanta@gmail.com>
2021-03-02 10:47:58 +0700

    Praktikum

---
+++ praktikum/tugas1/Latihan1a.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Muhammad Andrew Rahdi Rizanta
+203040159
+Jum'at 13.00-14.00
+*/
+?>
+<?php
+for( $i = 1; $i <= 3; $i++) {
+    
+    for ( $j = 1; $j <= 3; $j++) {
+        echo "Ini perulangan ke ($i,$j) <br>";
+    }
+    
+}
+
+
+?>
